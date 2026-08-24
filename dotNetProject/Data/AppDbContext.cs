d900a6386f89acf51701ccfb78f22830002df5cc
Neha <neha.panchal@rlogical.com>
2026-08-24 16:44:38 +0530

    DepartmentTable push

--- dotNetProject/Data/AppDbContext.cs
+++ dotNetProject/Data/AppDbContext.cs
@@ -11,5 +11,6 @@ namespace dotNetProject.Data
         }
 
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
     }
 }
--- dotNetProject/Migrations/20260824111355_AddDepartment.Designer.cs
+++ dotNetProject/Migrations/20260824111355_AddDepartment.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using dotNetProject.Data;
 
@@ -10,9 +11,11 @@ using dotNetProject.Data;
 namespace dotNetProject.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260824111355_AddDepartment")]
+    partial class AddDepartment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
