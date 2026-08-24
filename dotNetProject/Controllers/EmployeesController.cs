0cbcab4e1a237612c7b3f54757c82ae9704f0aa4
Neha <neha.panchal@rlogical.com>
2026-08-24 15:46:37 +0530

    commit new changes

---
+++ dotNetProject/Controllers/EmployeesController.cs
@@ -0,0 +1,41 @@
+﻿using dotNetProject.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotNetProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployees()
+        {
+            var employees = await _context.Employees.ToListAsync();
+
+            return Ok(employees);
+        }
+
+        // GET: api/employees/1
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployee(int id)
+        {
+            var employee = await _context.Employees
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (employee == null)
+            {
+                return NotFound("Employee not found");
+            }
+
+            return Ok(employee);
+        }
+    }
+}
